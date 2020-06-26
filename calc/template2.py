70043a31938f9c82b1b3616894021d85892f6a4e
yuujinkim <yuuuuu01@naver.com>
2020-06-26 21:47:56 +0900

    template2

---
+++ calc/template2.py
@@ -0,0 +1,16 @@
+html = """
+<html>
+    <body>
+        <form method="get" action="">
+            a = <input type="number" name="a">
+            b = <input type="number" name="b">
+
+            <input type="submit">
+        </form>
+        <p>
+        sum: %(sum)s</br>
+        product: %(product)s</br>
+        </p>
+    </body>
+</html>
+"""
